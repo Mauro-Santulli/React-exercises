c329221464dbf9081279ef9f348034a1fc89198a
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-11-22 22:26:57 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import HelloWorld from './HelloWorld';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+      <HelloWorld />
+);
+
+
