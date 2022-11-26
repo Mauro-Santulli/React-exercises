efbd96a77b71c954de1ee3e0f41346c343f55807
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-11-26 11:34:49 +0100

    Add exercise form 01

---
+++ App.js
@@ -0,0 +1,17 @@
+import { Counter } from "./Counter";
+import InteractiveWelcome from "./InteractiveWelcome";
+import {Welcome} from "./Welcome";
+
+// const nameElement="john"
+
+export function App() {
+
+
+    return (
+        <div>
+            <InteractiveWelcome/>
+            <Counter/>
+        </div>
+    )
+}
+
