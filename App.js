ef5e054701defbb58c53a71e78a286f8476a0f8b
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-11-30 23:38:28 +0100

    Add exercise use effect 03

---
+++ App.js
@@ -0,0 +1,12 @@
+import { GitHubUser } from "./GitHubUser";
+
+export default function App() {
+
+    return (
+        <div>
+            <GitHubUser username="Mauro-Santulli"/>
+        </div>
+    )
+
+}
+
---
+++ GitHubUser.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from "react";
+
+export function GitHubUser({ username }) {
+    const [data, setData] = useState();
+
+    useEffect(() => {
+        fetch(`https://api.github.com/users/${username}`)
+            .then((response) => {
+                return response.json()
+            })
+            .then((json) => {
+                console.log(json);
+                setData(json);
+            });
+    }, [username]);
+
+    return (
+        
+        <div>
+            {data && <h1>{data.login}</h1>}
+        </div>
+
+    )
+}
