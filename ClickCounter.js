0ac8daf0eb9b8aab37a682a1d69a7369c9829351
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-11-30 22:13:27 +0100

    Add exercise use state 02

---
+++ ClickCounter.js
@@ -0,0 +1,20 @@
+import React, { useState } from "react"
+import CounterDisplay from "./CounterDisplay"
+
+export default function ClickCounter({initialValue = 0}) {
+
+    const [count, setCount] = useState(initialValue)
+
+    function ClickButtonEvent(){
+        setCount(c=>c+1)
+        console.log(`counter: ${count}`)
+    }
+
+
+    return (
+        <div>
+            <CounterDisplay counter={count} />
+            <button onClick={ClickButtonEvent}>Increment Counter</button>
+        </div>
+    )
+}
