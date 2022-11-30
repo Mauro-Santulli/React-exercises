0d63464ccc5b4ed5c91ad2c812f0be78c2bf6cf7
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-11-30 22:56:24 +0100

    Add exercise use effect 02

---
+++ App.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import ClickCounter from "./ClickCounter";
+import Counter from "./Counter";
+import InteractiveWelcome from "./InteractiveWelcome";
+import { LanguageContext, DisplayLanguage } from "./LanguageContext";
+import Login from "./Login";
+import Sum from "./Sum";
+import TodoList from "./TodoList";
+
+export default function App() {
+
+    const [counter,setCounter]= useState(true)
+
+    function showCounter (){
+        setCounter(x=>!x)
+    }
+    return (
+        <div>
+            <button onClick={showCounter}>{counter ? 'Hide':'Show'}</button>
+            {counter && <Counter count={1} interval={500} amount={5}/>}
+
+        </div>
+    )
+
+}
+
