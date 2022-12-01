f7d3337bea662adc32837ad1f491dbcd7595fb7b
Mauro-Santulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-12-01 20:51:44 +0100

    Add exercise use ref 02

---
+++ App.js
@@ -0,0 +1,13 @@
+import { CardDetails } from "./CardDetails";
+
+
+export default function App() {
+
+    return (
+        <div>
+            <CardDetails car={{model: '500', year: 1999, color: 'red'}}/>
+        </div>
+    )
+
+}
+
