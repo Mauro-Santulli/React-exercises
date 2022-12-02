87eeb31a7286446ce0979f300815b82771db5c04
mauroSantulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-12-02 20:31:06 +0100

    add exercise react route 06

---
+++ App.js
@@ -0,0 +1,25 @@
+import { Welcome } from './Welcome';
+import { Routes, Route, Link } from 'react-router-dom'
+import { Counter } from './Counter';
+import { ShowGithubUser } from './ShowGithubUser';
+import { GithubUserList } from './GithubUserList';
+
+export default function App() {
+  return (
+    <div title="MyApp">
+      <Link to='/'>Home</Link> | <Link to='users'>User</Link> | <Link to='welcome'>Welcome</Link>
+      <Routes>
+        <Route path="/" element={<h2>My App</h2>} />
+        <Route path="*" element={<div><h1>Page Not Found</h1><Link to='/'>Go Back</Link></div>} />
+        <Route path="welcome" element={<Welcome />} />
+        <Route path="welcome/:name" element={<Welcome />} />
+        <Route path="counter" element={<Counter />} />
+        <Route path="/users" element={<GithubUserList />}>
+          <Route path="/users/:username" element={<ShowGithubUser />} />
+        </Route>
+      </Routes>
+    </div>
+  )
+}
+
+
---
+++ Welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import { useParams }from 'react-router-dom'
+
+export function Welcome() {
+
+    const {name='World'} = useParams()
+    
+    return (
+        <div>
+            <h1>Welcome {name} </h1>
+        </div>
+    )
+}
