dc9eed5f379cbe13dac4bb8f8d803bd21e1a2b12
mauroSantulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-12-02 23:31:51 +0100

    add component composition 02

---
+++ Container.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './index.css'
+
+export default function Container(props) {
+
+    return (<div className="border-2 border-red-600 border-solid"><h1>{props.title}</h1></div>)
+
+}
