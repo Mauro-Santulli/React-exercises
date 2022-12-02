e2dcc2c9630d096031295148c11f7733ce01ec0a
mauroSantulli <108669591+Mauro-Santulli@users.noreply.github.com>
2022-12-02 23:29:09 +0100

    add composition component 01

---
+++ App.js
@@ -0,0 +1,11 @@
+import Container from './Container';
+
+export default function App() {
+  return (
+    <div title="MyApp">
+      <Container />
+    </div>
+  )
+}
+
+
---
+++ Container.js
@@ -0,0 +1,8 @@
+import React from "react";
+import './index.css'
+
+export default function Container() {
+
+    return (<div className="border-2 border-red-600 border-solid"><h1>Hello World</h1></div>)
+
+}
